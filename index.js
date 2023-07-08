d4c30287cf00e4121d2fd443058668cba4e10f83
Cristina Alzuri <cristinaalzuri@gmail.com>
2023-07-08 14:13:54 -0400

    created the index.js file to set up the command-line interface and serve as the entry point of the application with prompts to handle user choices

--- index.js
+++ index.js
@@ -1,5 +1,71 @@
-// Required packages
-const mysql = require('mysql2');
-const inquirer = require("inquirer");
+// Import necessary modules and dependencies
+const inquirer = require('inquirer');
+const consoleTable = require('console.table');
+const {
+  viewAllDepartments,
+  viewAllRoles,
+  viewAllEmployees,
+  addDepartment,
+  addRole,
+  addEmployee,
+  updateEmployeeRole
+} = require('./app');
+
+// Prompt the user with options and handle their choices
+const promptUser = () => {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'action',
+      message: 'What would you like to do?',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Exit'
+      ]
+    }
+  ]).then((answers) => {
+    switch (answers.action) {
+      case 'View all departments':
+        viewAllDepartments();
+        break;
+      case 'View all roles':
+        viewAllRoles();
+        break;
+      case 'View all employees':
+        viewAllEmployees();
+        break;
+      case 'Add a department':
+        addDepartment();
+        break;
+      case 'Add a role':
+        addRole();
+        break;
+      case 'Add an employee':
+        addEmployee();
+        break;
+      case 'Update an employee role':
+        updateEmployeeRole();
+        break;
+      case 'Exit':
+        console.log('Goodbye!');
+        process.exit(0);
+    }
+  });
+};
+
+// Start the application
+const init = () => {
+  console.log('Welcome to the Employee Tracker System!');
+  promptUser();
+};
+
+// Initialize the application
+init();
 
 
